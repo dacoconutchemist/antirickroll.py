c7c7e0a315543ec3d3ba8b80c25facf74a0b916e
dacoconutchemist <75437100+dacoconutchemist@users.noreply.github.com>
2021-05-17 11:12:49 +0300

    added support for links in <> brackets, optimized code length
    this is being used for a discord bot, links in <> in discord do not display their content

--- antirickroll.py
+++ antirickroll.py
@@ -1,44 +1,40 @@
 import requests
+from functools import lru_cache
 
 #            name        song name                  surname   link st  link st 2    link st 3  link st 4    alternative
 keywords = ["rickroll", "never gonna give you up", "astley", "dqw4w9", "iik25wqi", "d0tgbcc", "xvfzjo5pg", "wreck roll"]
 
+@lru_cache(maxsize=32)
 def rickroll(link):
     '''Description: checks if a link is a rickroll. Check antirickroll.keywords for their list. Returns None if link invalid. 
     WARNING: Can take long if there are many redirects, run this with asyncio!!!'''
-    
-    request = ""
-    if "9saKj_npn" in link: #exception for my own rickroll video MUHAHAHAHA
-        return False
-    try:
-        request = requests.get(link)
-    except (requests.URLRequired, requests.exceptions.MissingSchema, requests.exceptions.InvalidSchema, requests.exceptions.InvalidURL):
-        return None
-    t = request.text.lower()
     global keywords
-    for i in keywords:
-        if i in t:
-            return True
-    return False
+    request = ""
+    if "9saKj_npn" in link: return False
+    if any(kw in link for kw in keywords): return True
+    try: request = requests.get(link)
+    except (requests.URLRequired, requests.exceptions.MissingSchema, requests.exceptions.InvalidSchema, requests.exceptions.InvalidURL): return None
+    t = request.text.lower()
+    return any(i in t for i in keywords)
 
 def findLinks(text):
     '''Description: Finds all links in a piece of text. 
     WARNING: Can be false activated (rarely)!!! Check links with the requests library!'''
-    srt = text
-    srt += " "
+    text += " "
+    text = text.replace("\n", " ")
     linx = []
-    def shortDaCode(c):
+    def shortDaCode(c, cc):
         try:
-            opt1 = srt.split("http")
-            for i in range(1, len(srt.split("http"))):
+            opt1 = text.split(cc)
+            for i in range(1, len(opt1)):
                 appd = opt1[i].split(c)[0]
-                if (" " in appd) or ("\n" in appd):
-                    continue
+                if " " in appd or ">" in appd: continue
                 linx.append("http" + appd)
         except IndexError:
             pass
-    shortDaCode(" ")
-    shortDaCode("\n")
+    shortDaCode(" ", "http")
+    shortDaCode(">", "<http")
+    #shortDaCode("\n")
     return linx
 
 def findRickrolls(srt):
@@ -66,6 +62,10 @@ if __name__ == "__main__":
 https://DryRespectfulSystemsanalysis-1.dacoconutnut.repl.co
 https://www.youtube.com/watch?v=9saKj_npnYo
 http protocol rickroll
+<https://www.youtube.com/watch?v=gpg8g_JssGc>
 """
     print("test string: " + testString)
+    import time
+    start_time = time.time()
     pprint(findRickrolls(testString))
+    print("--- %s seconds ---" % (time.time() - start_time))
